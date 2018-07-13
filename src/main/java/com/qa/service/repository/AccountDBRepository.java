a27255d91c589b461a3e430b43fe72169895c462
Kate <e.genova@live.co.uk>
2018-07-13 15:57:23 +0100

    repo impl

--- src/main/java/com/qa/service/repository/AccountDBRepository.java
+++ src/main/java/com/qa/service/repository/AccountDBRepository.java
@@ -1,25 +1,77 @@
 package com.qa.service.repository;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+import java.util.Collection;
+
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+
+import com.qa.domain.Account;
+import com.qa.util.JSONutil;
+
+@Transactional(SUPPORTS)
+@Default
 public class AccountDBRepository implements AccountRepository {
 
+	@PersistenceContext(unitName = "primary")
+	private EntityManager manager;
+	
+	@Inject
+	private JSONutil util;
+	
+	@Override
 	public String getAllAccounts() {
-		// TODO Auto-generated method stub
-		return null;
+		Query query = manager.createQuery("SELECT a FROM Account a");
+		Collection<Account> accounts = (Collection<Account>) query.getResultList();
+		return util.getJSONForObject(accounts);
 	}
 
-	public String createAccount(String accout) {
-		// TODO Auto-generated method stub
-		return null;
+	@Override
+	@Transactional(REQUIRED)
+	public String createAccount(String account) {
+		Account anAccount = util.getObjectForJSON(account, Account.class);
+		manager.persist(anAccount);
+		return "{\"message\": \"account has been sucessfully added\"}";
 	}
 
+	@Override
+	@Transactional(REQUIRED)
 	public String updateAccount(Long id, String accountToUpdate) {
-		// TODO Auto-generated method stub
-		return null;
+		Account updatedAccount = util.getObjectForJSON(accountToUpdate, Account.class);
+		Account accountFromDB = findAccount(id);
+		if (accountToUpdate != null) {
+			accountFromDB = updatedAccount;
+			manager.merge(accountFromDB);
+		}
+		return "{\"message\": \"account sucessfully updated\"}";
 	}
 
+	@Override
+	@Transactional(REQUIRED)
 	public String deleteAccount(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Account accountInDB = findAccount(id);
+		if (accountInDB != null) {
+			manager.remove(accountInDB);
+		}
+		return "{\"message\": \"account sucessfully deleted\"}";
+	}
+	
+	private Account findAccount(Long id) {
+		return manager.find(Account.class, id);
+	}
+	
+	public void setManager(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	public void setUtil(JSONutil util) {
+		this.util = util;
 	}
 
 }
---
+++ src/main/java/com/qa/util/JSONutil.java
@@ -0,0 +1,19 @@
+package com.qa.util;
+
+import java.util.Collection;
+
+import com.qa.domain.Account;
+
+public class JSONutil {
+
+	public Account getObjectForJSON(String account, Class<Account> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getJSONForObject(Collection<Account> accounts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
