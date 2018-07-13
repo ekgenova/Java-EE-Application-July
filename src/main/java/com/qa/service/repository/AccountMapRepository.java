9c4a74b24cf1906fe9a358f6b8badfd45a5883b8
Kate <e.genova@live.co.uk>
2018-07-13 14:42:29 +0100

    repo init

---
+++ src/main/java/com/qa/service/repository/AccountMapRepository.java
@@ -0,0 +1,25 @@
+package com.qa.service.repository;
+
+public class AccountMapRepository implements AccountRepository {
+
+	public String getAllAccounts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String createAccount(String accout) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String updateAccount(Long id, String accountToUpdate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String deleteAccount(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
